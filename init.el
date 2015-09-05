edfa11bcad11a7be4bf0016a5c6407bb0dc8d387
cg82616424 <cg82616424@sina.com>
2015-09-05 22:31:19 +0800

    add python company conf

---
+++ init.el
@@ -0,0 +1,20 @@
+(add-to-list 'load-path
+              "~/.emacs.d/plugins/yasnippet")
+(add-to-list 'load-path
+              "~/.emacs.d/plugins/company-mode")
+;;load some plugins
+(require 'yasnippet)
+(yas-global-mode 1)
+;;yasnippet conf
+(autoload 'company-mode "company" nil t)
+;;c mode c++ mode 
+(add-hook 'c-mode-hook '(lambda () (company-mode)))
+(add-hook 'c++-mode-hook '(lambda () (company-mode)))
+(add-hook 'python-mode-hook '(lambda () (company-mode)))
+;;company mode conf
+;;python mode jedi company conf
+;;(defun my/python-mode-hook ()
+;;  (add-to-list 'company-backends 'company-jedi))
+;;(add-hook 'python-mode-hook 'my/python-mode-hook)
+
+
