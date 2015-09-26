fa3bbcbc8dcb3e466b13d19356cf4ca22f3aeff5
cg82616424 <cg82616424@sina.com>
2015-09-26 23:39:40 +0800

    1. add elisp mode hook 2. add php mode hook 3. auto open line  num

--- init.el
+++ init.el
@@ -2,16 +2,22 @@
 (add-to-list 'load-path
               "~/.emacs.d/plugins/yasnippet")
 (add-to-list 'load-path
-              "~/.emacs.d/plugins/company-mode")
+	     "~/.emacs.d/plugins/company-mode")
+(add-to-list 'load-path
+              "~/.emacs.d/plugins/php-mode")
 ;;load some plugins
 (require 'yasnippet)
+(require 'php-mode)
 (yas-global-mode 1)
+(global-linum-mode 1)
 ;;yasnippet conf
 (autoload 'company-mode "company" nil t)
 ;;c mode c++ mode 
 (add-hook 'c-mode-hook '(lambda () (company-mode)))
 (add-hook 'c++-mode-hook '(lambda () (company-mode)))
 (add-hook 'python-mode-hook '(lambda () (company-mode)))
+(add-hook 'elisp-mode-hook '(lambda () (elisp-mode)))
+(add-hook 'php-mode-hook '(lambda () (php-mode)))
 ;;company mode conf
 ;;python mode jedi company conf
 (defun run-python-once ()
@@ -20,3 +26,4 @@
 
 (add-hook 'python-mode-hook 'run-python-once)
 ;;auto run a python process in python mode
+
